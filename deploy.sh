047825a9ff60ee52d9a28d0eb4fbe7f1c5321dff
Chikelue Oji <chike_oji@yahoo.com>
2019-02-12 16:57:39 +0100

    Extending the deployment with multiple(X2) instances/containers and a load balancer

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,9 @@ echo "yes" | terraform apply
 ansible_configure() {
 echo "Waiting for 300 seconds to allow for the EC2 machine to be fully provisioned and to come online"
 sleep 300
+
 ansible-playbook -i $ROOT_DIR/ansible/hosts $ROOT_DIR/ansible/main.yml -e 'ansible_python_interpreter=/usr/bin/python3'
+
 }
 
 while true; do
@@ -45,15 +47,15 @@ echo "-----------------------------------------------------------------------"
 echo " Dockerize the Ceros Ski NodeJS app and push to ECR Container Registry "
 echo "-----------------------------------------------------------------------"
 
-npm install
+# npm install
 
-$(aws ecr get-login --no-include-email --region us-east-2)
+# $(aws ecr get-login --no-include-email --region us-east-2)
 
-docker build -t ceros/ski .
+# docker build -t ceros/ski .
 
-docker tag ceros/ski:latest 519826568739.dkr.ecr.us-east-2.amazonaws.com/ceros/ski:latest
+# docker tag ceros/ski:latest 519826568739.dkr.ecr.us-east-2.amazonaws.com/ceros/ski:latest
 
-docker push 519826568739.dkr.ecr.us-east-2.amazonaws.com/ceros/ski:latest
+# docker push 519826568739.dkr.ecr.us-east-2.amazonaws.com/ceros/ski:latest
 
 
 # Setting up the resources for maintaining and locking the terraform remote state
@@ -61,8 +63,12 @@ echo " Setting up the resources for maintaining and locking the terraform remote
 cd terraform/remote-state/
 terraform init && terraform fmt && terraform validate 
 echo "yes" | terraform apply -var-file="starter-vars.tfvars"
-# terraform show
+terraform show
 cd ../../
+
+# Clear the hosts file for another deployment session
+echo "Clearing the hosts file for another deployment session"
+> $ROOT_DIR/ansible/hosts
 
 while true; do
     read -p "Do you wish to proceed with the server provisioning? " yn
--- terraform/instance.tf
+++ terraform/instance.tf
@@ -49,22 +49,75 @@ resource "aws_security_group" "ceros-allow-inbound-app" {
   }
 }
 
+resource "aws_security_group" "ceros-allow-bidirectional-http" {
+  name        = "ceros-allow-bidirectional-http"
+  description = "Ceros security group that allows bi-directional http traffic to and from the Nginx load balancer"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "ceros-allow-bidirectional-http"
+  }
+}
+
 resource "aws_key_pair" "ceros_id_rsa" {
   key_name   = "ceros_id_rsa"
   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
 }
 
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "ceros-ski-appserver1" {
   ami                    = "${lookup(var.AMI_ID, var.AWS_REGION)}"
   instance_type          = "t2.micro"
   key_name               = "${aws_key_pair.ceros_id_rsa.key_name}"
   vpc_security_group_ids = ["${aws_security_group.ceros-allow-inbound-ssh.id}", "${aws_security_group.ceros-allow-outbound-internet.id}", "${aws_security_group.ceros-allow-inbound-app.id}"]
 
   tags {
-    Name = "Ubuntu-16.04"
+    Name = "Ceros Ski Appserver 1 Ubuntu-16.04"
   }
 
   provisioner "local-exec" {
-    command = "echo \"[ceros-ski-server]\n${aws_instance.ubuntu.public_ip} ansible_connection=ssh ansible_ssh_user=ubuntu\"  > ../ansible/hosts"
+    command = "echo \"[ceros-ski-appserver1]\n${aws_instance.ceros-ski-appserver1.public_ip} ansible_connection=ssh ansible_ssh_user=ubuntu\"  >> ../ansible/hosts; echo \"${aws_instance.ceros-ski-appserver1.public_ip}\"  > ../ansible/ceros-ski-appserver1-hosts"
+  }
+}
+
+resource "aws_instance" "ceros-ski-appserver2" {
+  ami                    = "${lookup(var.AMI_ID, var.AWS_REGION)}"
+  instance_type          = "t2.micro"
+  key_name               = "${aws_key_pair.ceros_id_rsa.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.ceros-allow-inbound-ssh.id}", "${aws_security_group.ceros-allow-outbound-internet.id}", "${aws_security_group.ceros-allow-inbound-app.id}"]
+
+  tags {
+    Name = "Ceros Ski Appserver 2 Ubuntu-16.04"
+  }
+
+  provisioner "local-exec" {
+    command = "echo \"[ceros-ski-appserver2]\n${aws_instance.ceros-ski-appserver2.public_ip} ansible_connection=ssh ansible_ssh_user=ubuntu\"  >> ../ansible/hosts; echo \"${aws_instance.ceros-ski-appserver2.public_ip}\"  > ../ansible/ceros-ski-appserver2-hosts"
+  }
+}
+
+resource "aws_instance" "ceros-ski-loadbalancer" {
+  ami                    = "${lookup(var.AMI_ID, var.AWS_REGION)}"
+  instance_type          = "t2.micro"
+  key_name               = "${aws_key_pair.ceros_id_rsa.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.ceros-allow-inbound-ssh.id}", "${aws_security_group.ceros-allow-outbound-internet.id}", "${aws_security_group.ceros-allow-bidirectional-http.id}"]
+
+  tags {
+    Name = "Ceros Ski Loadbalancer Ubuntu-16.04"
+  }
+
+  provisioner "local-exec" {
+    command = "echo \"[ceros-ski-loadbalancer]\n${aws_instance.ceros-ski-loadbalancer.public_ip} ansible_connection=ssh ansible_ssh_user=ubuntu\"  >> ../ansible/hosts"
   }
 }
